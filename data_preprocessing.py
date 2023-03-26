1cfec1c653c4b62c49ac32a69f577196da373212
krishn64 <72079144+krishn64@users.noreply.github.com>
2023-03-26 15:46:07 +0530

    Add files via upload

---
+++ data_preprocessing.py
@@ -0,0 +1,13 @@
+import csv
+
+with open("final_1.csv", "r") as source:
+    reader = csv.reader(source) 
+    with open("final_2.csv", "w") as result:
+        writer = csv.writer(result)
+        for row in reader:
+            if row!='':
+                writer.writerow((row[0], row[1], row[2], row[3], row[4],row[5], row[6], row[9]))
+            else:
+                pass
+
+
